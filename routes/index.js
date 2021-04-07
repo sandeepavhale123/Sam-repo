bf41fe73ce64b62ce890030a7c3574563e9d9ebc
sandeepavhale123 <59818209+sandeepavhale123@users.noreply.github.com>
2021-04-07 14:33:38 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-var pgp = require('pg-promise')(/* options */);
-
 router.get('/', function (req, res) {
   res.json({ title: 'welcome home' });
-});
-
-router.get('/posts', function (req, res) {
-  db.query('SELECT * FROM posts')
-    .then(function (data) {
-      res.json({ posts: data });
-    })
-    .catch(function (error) {
-      console.log('Error', error);
-      res.json({ error: error });
-    });
 });
 
 module.exports = router;
